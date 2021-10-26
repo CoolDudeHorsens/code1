4837cd17145f6493b65b4ac57113433f0852ac25
CoolDudeHorsens <91544793+CoolDudeHorsens@users.noreply.github.com>
2021-10-26 13:35:04 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+/*
+function introduceMe (name:string, age:number){
+    console.log ("Hi my name is " + name +  " and my age is " + age)
+} 
+introduceMe ("Oscar",17) // write in console the values introduceMe
+
+
+function introduceMe2(name: string, age: number) {
+    return "Hi my name is " + name + " and my age is " + age
+}
+console.log (introduceMe2("Oscar", 18)) // write in console the return
+
+
+let introduction = introduceMe2("Oscar",19) // variable
+console.log(introduction) // write in console
+*/
+
+let list = ["James","Peter","Jakob","Stine"]
+for (let i=0;i<list.length;i++){
+    console.log(list[i])
+}
